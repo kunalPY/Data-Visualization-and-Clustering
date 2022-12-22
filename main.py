8e30ce9a1777b913c1153fa1a0c8039e25b703c9
kunalPY <70991754+kunalPY@users.noreply.github.com>
2022-12-21 16:24:22 -0800

    Added functionality
    It uses the KMeans algorithm from the scikit-learn library to cluster the data into groups based on the values in two columns.
    It adds a new column to the dataframe to store the cluster assignment for each row.
    It creates a scatterplot to visualize the clusters using Matplotlib.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
 
 def analyze_data(data_path):
   # Load the data into a Pandas dataframe
@@ -19,6 +21,17 @@ def analyze_data(data_path):
 
   # Create a histogram to visualize the distribution of the data
   df.hist(column='column_name', bins=50)
+  plt.show()
+  
+  # Use K-Means clustering to group the data into clusters
+  kmeans = KMeans(n_clusters=5)
+  kmeans.fit(df[['column_name_1', 'column_name_2']])
+  df['cluster'] = kmeans.predict(df[['column_name_1', 'column_name_2']])
+
+  # Create a scatterplot to visualize the clusters
+  plt.scatter(df['column_name_1'], df['column_name_2'], c=df['cluster'], cmap='viridis')
+  plt.xlabel('Column Name 1')
+  plt.ylabel('Column Name 2')
   plt.show()
 
 # Example usage
